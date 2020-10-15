e73b52f935bcde4375555febe83934ba15fbb3ea
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-10-15 11:12:17 -0700

    WIP pagination

--- lib/live_view_studio_web/live/paginate_live.ex
+++ lib/live_view_studio_web/live/paginate_live.ex
@@ -3,63 +3,23 @@ defmodule LiveViewStudioWeb.PaginateLive do
 
   alias LiveViewStudio.Donations
 
-  def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
-
-    socket =
-      assign(socket,
-        donations: donations
+  def mount(params, _session, socket) do
+    donations =
+      Donations.list_donations(
+        paginate: page_opts(params),
+        sort: %{sort_by: :item, sort_order: :asc}
       )
+
+    socket = assign(socket, donations: donations, options: page_opts(params))
 
     {:ok, socket, temporary_assigns: [donations: []]}
   end
 
-  def render(assigns) do
-    ~L"""
-    <h1>Food Bank Donations</h1>
-    <div id="donations">
-      <div class="wrapper">
-        <table>
-          <thead>
-            <tr>
-              <th class="item">
-                Item
-              </th>
-              <th>
-                Quantity
-              </th>
-              <th>
-                Days Until Expires
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <%= for donation <- @donations do %>
-              <tr>
-                <td class="item">
-                  <span class="id"><%= donation.id %></span>
-                  <%= donation.emoji %> <%= donation.item %>
-                </td>
-                <td>
-                  <%= donation.quantity %> lbs
-                </td>
-                <td>
-                  <span class="<%= expires_class(donation) %>">
-                    <%= donation.days_until_expires %>
-                  </span>
-                </td>
-              </tr>
-            <% end %>
-          </tbody>
-        </table>
-        <div class="footer">
-          <div class="pagination">
-          </div>
-        </div>
-      </div>
-    </div>
-    """
+  defp page_opts(%{"page" => page, "per_page" => per_page}) do
+    %{page: String.to_integer(page), per_page: String.to_integer(per_page)}
   end
+
+  defp page_opts(_params), do: %{page: 1, per_page: 5}
 
   defp expires_class(donation) do
     if Donations.almost_expired?(donation), do: "eat-now", else: "fresh"
