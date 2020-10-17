afb7cb294f55ce707ddffc559c9ffa06b4257834
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-10-17 10:06:49 -0700

    WIP pagination 3

--- lib/live_view_studio_web/live/paginate_live.ex
+++ lib/live_view_studio_web/live/paginate_live.ex
@@ -3,11 +3,11 @@ defmodule LiveViewStudioWeb.PaginateLive do
 
   alias LiveViewStudio.Donations
 
-  def mount(params, _session, socket) do
+  def mount(_params, _session, socket) do
     {:ok, socket, temporary_assigns: [donations: []]}
   end
 
-  # NOTE: always invoked after `mout/3`
+  # NOTE: always invoked after `mount/3`
   def handle_params(params, _uri, socket) do
     {:noreply,
      assign(
@@ -17,6 +17,12 @@ defmodule LiveViewStudioWeb.PaginateLive do
      )}
   end
 
+  def handle_event("select-per-page", params, socket) do
+    socket = assign(socket, options: page_opts(params))
+    path = to_page(nil, page_opts(params), socket)
+    {:noreply, push_patch(socket, to: path)}
+  end
+
   defp donations(params) do
     Donations.list_donations(
       paginate: page_opts(params),
@@ -24,22 +30,34 @@ defmodule LiveViewStudioWeb.PaginateLive do
     )
   end
 
-  defp page_opts(%{"page" => page, "per_page" => per_page}) do
-    %{page: String.to_integer(page), per_page: String.to_integer(per_page)}
-  end
-
-  defp page_opts(_params), do: %{page: 1, per_page: 5}
-
   defp expires_class(donation) do
     if Donations.almost_expired?(donation), do: "eat-now", else: "fresh"
   end
 
-  defp to_page(direction, _opts = %{page: page, per_page: per_page}, socket) do
-    Routes.live_path(
-      socket,
-      __MODULE__,
-      page: (if direction == :prev, do: page - 1, else: page + 1),
-      per_page: per_page
-    )
+  def page_opts(%{"page" => page, "per-page" => per_page}) do
+    %{page: String.to_integer(page), per_page: String.to_integer(per_page)}
+  end
+
+  def page_opts(%{"per_page" => per_page}) do
+    %{page: 1, per_page: String.to_integer(per_page)}
+  end
+
+  def page_opts(%{"page" => page}) do
+    %{page: String.to_integer(page), per_page: 5}
+  end
+
+  # default
+  def page_opts(_params), do: %{page: 1, per_page: 5}
+
+  defp to_page(nil, %{page: page, per_page: per_page}, socket) do
+    Routes.live_path(socket, __MODULE__, page: page, per_page: per_page)
+  end
+
+  defp to_page(:next, %{page: page, per_page: per_page}, socket) do
+    Routes.live_path(socket, __MODULE__, page: page + 1, per_page: per_page)
+  end
+
+  defp to_page(:prev, %{page: page, per_page: per_page}, socket) do
+    Routes.live_path(socket, __MODULE__, page: page - 1, per_page: per_page)
   end
 end
