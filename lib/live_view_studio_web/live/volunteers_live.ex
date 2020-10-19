815520dfc895e88b1d2fb4785d029f2c82116860
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2020-10-18 21:25:22 -0700

    form create
    - phx-update
      - replace
      - prepend
      - append

--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -1,17 +1,36 @@
 defmodule LiveViewStudioWeb.VolunteersLive do
   use LiveViewStudioWeb, :live_view
 
-  alias LiveViewStudio.Volunteers
-  alias LiveViewStudio.Volunteers.Volunteer
+  alias LiveViewStudio.{Volunteers, Volunteers.Volunteer}
 
   def mount(_params, _session, socket) do
     volunteers = Volunteers.list_volunteers()
 
+    changeset = Volunteers.change_volunteer(%Volunteer{})
+
     socket =
       assign(socket,
-        volunteers: volunteers
+        volunteers: volunteers,
+        changeset: changeset
       )
 
-    {:ok, socket}
+    # NOTE: the temporary assigns helps us free up memory once the view is
+    # rendered by not keeping the volunteers in memory in our genserver
+    {:ok, socket, temporary_assigns: [volunteers: []]}
+  end
+
+  def handle_event("save", %{"volunteer" => params}, socket) do
+    case Volunteers.create_volunteer(params) do
+      {:ok, volunteer} ->
+        socket =
+          socket
+          |> update(:volunteers, &[volunteer | &1])
+          |> assign(changeset: Volunteers.change_volunteer(%Volunteer{}))
+
+        {:noreply, socket}
+
+      {:error, changeset = %Ecto.Changeset{}} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
   end
 end
